f1e08f0862ea4720ab09c87630d6ad6edb5fc27b
Stanley-Felix-Bergamo <stanley.felix.bergamo@outlook.com>
2024-04-21 20:10:56 -0300

    commit

---
+++ src/components/Form/FileInput/ImagePreview.tsx
@@ -0,0 +1,9 @@
+import { AiOutlineUser } from "react-icons/ai";
+
+export function ImagePreview(){
+    return (
+         <div className="bg-violet-50 flex h-16 w-16 items-center justify-center rounded-full">
+             <AiOutlineUser className="w-8 h-8 text-violet-500" />
+         </div>                                       
+    )
+}
