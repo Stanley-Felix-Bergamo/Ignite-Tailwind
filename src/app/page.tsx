3e8004055d67e6ad1edcdd3d398c7a6c1f72dd75
Stanley-Felix-Bergamo <stanley.felix.bergamo@outlook.com>
2024-04-21 12:47:00 -0300

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { SettingsTabs } from "@/components/SettingsTabs";
 import * as Input from '@/components/Input'
 import { BsEnvelope } from "react-icons/bs";
+import { AiOutlineUser,AiOutlineCloudUpload } from "react-icons/ai";
 
 
 export default function Home() {
@@ -46,10 +47,29 @@ export default function Home() {
                 </div>
 
                 <div className="grid grid-cols-form gap-3 pt-5">
-                  <label htmlFor="email" className="text-sm font-medium text-zinc-700">Your photo
+                  <label htmlFor="photo" className="text-sm font-medium text-zinc-700">Your photo
                   <span className="mt-0.5 block text-sm font-normal text-zinc-500">This will be displayed on your profile.</span>
                   </label>                                     
-                  <div></div>
+                  <div className="flex items-start gap-5">
+                    <div className="bg-violet-50 flex h-16 w-16 items-center justify-center rounded-full">
+                      <AiOutlineUser className="w-8 h-8 text-violet-500" />
+                    </div>
+                    
+                    <label htmlFor="photo" className="group flex-1 cursor-pointer flex flex-col items-center gap-3 rounded-lg border border-zinc-300 px-6 py-4 text-center text-zinc-500 shadow-sm hover:border-violet-200 hover:bg-violet-25 hover:text-violet-500">
+                      <div className="rounded-full border-6 border-zinc-50 bg-zinc-100 p-2 group-hover:border-violet-50 group-hover:bg-violet-100">
+                          <AiOutlineCloudUpload className="h-5 w-5 text-zinc-600 group-hover:text-violet-600" />
+                      </div>
+                      <div className="flex flex-col items-center gap-1">
+                          <span className="text-sm">
+                            <span className="font-semibold text-violet-700">Click to upload</span> or drag and drop                             
+                            </span>
+                          <span className="text-xs">SVG, PNG, JPG or GIF (max. 800x400px)</span>
+                      </div>
+                    </label>
+
+                    <input type="file" className="sr-only" id="photo" />
+
+                  </div>
                 </div>
                 
                <div className="grid grid-cols-form gap-3 pt-5">
@@ -59,18 +79,15 @@ export default function Home() {
                     </Input.InputRoot>                                           
                </div>
 
-
                <div className="grid grid-cols-form gap-3 pt-5">
                   <label htmlFor="country" className="text-sm font-medium text-zinc-700">Country</label>                                            
                    <div></div>                                          
                </div>
 
-
                <div className="grid grid-cols-form gap-3 pt-5">
                   <label htmlFor="timezone" className="text-sm font-medium text-zinc-700">Timezone</label>                                            
                    <div></div>                                          
                </div>
-       
 
                 <div className="grid grid-cols-form gap-3 pt-5">
                   <label htmlFor="bio" className="text-sm font-medium text-zinc-700">Bio
@@ -79,7 +96,6 @@ export default function Home() {
                   <div></div>
                 </div>
 
-
                 <div className="grid grid-cols-form gap-3 pt-5">
                   <label htmlFor="projects" className="text-sm font-medium text-zinc-700">Portfolio projects
                   <span className="mt-0.5 block text-sm font-normal text-zinc-500">Share a few snippets of your work.</span>
@@ -87,13 +103,10 @@ export default function Home() {
                   <div></div>
                 </div>
 
-
-
                 <div className="flex items-center justify-end gap-2 pt-5mit">
                   <button type="button" className="rounded-lg border border-zinc-300 px-4 py-2 text-sm font-semibold shadow-sm text-zinc-700 hover:bg-zinc-50">Cancel</button>
                   <button type="button" className="rounded-lg px-4 py-2 text-sm font-semibold shadow-sm bg-violet-600 text-white hover:bg-violet-700 ">Save</button>
                 </div>
-               
               </form>
             </div> 
         </>
--- src/components/SettingsTabs/TabItem.tsx
+++ src/components/SettingsTabs/TabItem.tsx
@@ -9,10 +9,10 @@ export interface TabItemProps{
 
 export function TabItem({title,value,isSelected = false}:TabItemProps) {
     return(
-         <Tabs.Trigger value={value} className='px-1 pb-4 font-medium text-zinc-500 hover:text-violet-700 data-[state=active]:text-violet-500'>
+         <Tabs.Trigger value={value} className='relative px-1 pb-4 text-sm font-medium text-zinc-500 hover:text-violet-700 data-[state=active]:text-violet-700'>
             <span>{title}</span>
             {isSelected &&(
-                 <motion.div layoutId='activeTab' className=' -bottom-px left-0 right-0 h-0.5 bg-violet-500 '/>
+                 <motion.div layoutId='activeTab' className='absolute -bottom-px left-0 right-0 h-0.5 bg-violet-500 '/>
             )}
           </Tabs.Trigger>
     )
