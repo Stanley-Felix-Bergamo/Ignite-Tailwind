c207a595101ef827beef5bf79aed076c1c37c203
Stanley-Felix-Bergamo <stanley.felix.bergamo@outlook.com>
2024-04-24 13:10:15 -0300

    commit

--- src/components/Sidebar/Profile.tsx
+++ src/components/Sidebar/Profile.tsx
@@ -6,10 +6,10 @@ export function Profile(){
         <div className="grid items-center gap-3 grid-cols-profile">
             <img src="https://github.com/stanley-felix-bergamo.png" className="h-10 w-10 rounded-full" alt="" />
              <div className="flex flex-1 flex-col truncate">
-                <span className="text-sm font-semibold text-zinc-700">
+                <span className="text-sm font-semibold text-zinc-700 dark:text-zinc-100">
                     Stanley
                 </span>
-                <span className="truncate text-sm text-zinc-500">stanley.felix.bergamo@outlook.com</span>
+                <span className="truncate text-sm text-zinc-500 dark:text-zinc-400">stanley.felix.bergamo@outlook.com</span>
              </div>
             <Button type="button" variant="ghost">
                 <BiExit className="h-5 w-5 text-zinc-500" />
