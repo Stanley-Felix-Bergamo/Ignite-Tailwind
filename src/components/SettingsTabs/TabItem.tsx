5ec820cec7b540c0bf98b6cd8fe8d954f04136b1
Stanley-Felix-Bergamo <stanley.felix.bergamo@outlook.com>
2024-04-24 09:04:44 -0300

    commit

--- src/components/SettingsTabs/TabItem.tsx
+++ src/components/SettingsTabs/TabItem.tsx
@@ -10,7 +10,7 @@ export interface TabItemProps{
 export function TabItem({title,value,isSelected = false}:TabItemProps) {
     return(
          <Tabs.Trigger value={value} className='relative px-1 pb-4 text-sm font-medium text-zinc-500 hover:text-violet-700 data-[state=active]:text-violet-700'>
-            <span>{title}</span>
+            <span className="whitespace-nowrap rounded group-focus-visible:ring-2 group-focus-visible:ring-violet-400 group-focus-visible:ring-offset-4">{title}</span>
             {isSelected &&(
                  <motion.div layoutId='activeTab' className='absolute -bottom-px left-0 right-0 h-0.5 bg-violet-500 '/>
             )}
